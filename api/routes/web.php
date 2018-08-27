1a3eb8e77a2e2147f662d4fb6ffde97d4cc24491
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-27 19:53:40 -0300

    [ADD] Criação de controller inicial para trabalhar com Usuario
    Co-authored-by: Gabriel Avaloni <gabrielavaloni@gmail.com>

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Http\Response;
 
 $router->get('/', function () use ($router) {
+    return redirect()->route('api');
+});
+
+$router->get('/api', ['as' => 'api', function () use ($router) {
     return response()->json(
         [
             'name' => 'Abigail', 
             'state' => 'CA'
         ]
     );
-});
+}]);
+
+$router->get('/user[/{id}]', 'UsuarioController@get');
