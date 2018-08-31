5d8c4aa524b4d88015e7ae063c94e6b040a35d09
vinnyfs89 <viniciusfesil@gmail.com>
2018-08-31 11:38:01 -0300

    [UPDATE] definição de grupo para rotas de acordo com pattern da versão da API

--- api/routes/web.php
+++ api/routes/web.php
@@ -15,12 +15,19 @@ $router->get('/api', ['as' => 'api', function () use ($router) {
     );
 }]);
 
-$router->get('/conta[/{id}]', 'ContaController@get');
-$router->post('/conta', 'ContaController@post');
-$router->patch('/conta/{id}', 'ContaController@patch');
-$router->delete('/conta/{id}', 'ContaController@delete');
+define('API_VERSION', '1.0');
+define('API_PATTERN', 'v1');
 
-$router->get('/plataforma[/{id}]', 'PlataformaController@get');
-$router->post('/plataforma', 'PlataformaController@post');
-$router->patch('/plataforma/{id}', 'PlataformaController@patch');
-$router->delete('/plataforma/{id}', 'PlataformaController@delete');
+$app->group(['prefix' => API_PATTERN], function () use ($router) {
+    $router->get('/conta[/{id}]', 'ContaController@get');
+    $router->post('/conta', 'ContaController@post');
+    $router->patch('/conta/{id}', 'ContaController@patch');
+    $router->delete('/conta/{id}', 'ContaController@delete');
+
+    $router->get('/plataforma[/{id}]', 'PlataformaController@get');
+    $router->post('/plataforma', 'PlataformaController@post');
+    $router->patch('/plataforma/{id}', 'PlataformaController@patch');
+    $router->delete('/plataforma/{id}', 'PlataformaController@delete');
+
+});
+
