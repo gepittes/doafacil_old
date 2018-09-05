8c306a4abbc6026f7116c7a8cf83fa5693889143
vinnyfs89 <viniciusfesil@gmail.com>
2018-09-05 18:36:22 -0300

    [PATCH] Criação de scripts para popular o banco de dados.
    Co-authored-by: Gabriel Avaloni <gabrielavaloni@gmail.com>

---
+++ database/initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO notificacao.usuario (usuario_id, descricao, is_ativo) VALUES (1, 'usuario1', true);
+INSERT INTO notificacao.plataforma (plataforma_id, descricao, is_ativo) VALUES (1, 'plataforma web', true);
+INSERT INTO notificacao.plataforma (plataforma_id, descricao, is_ativo) VALUES (2, 'plataforma 2', true);
+INSERT INTO notificacao.sistema (sistema_id, descricao, is_ativo) VALUES (1, 'salic', true);
