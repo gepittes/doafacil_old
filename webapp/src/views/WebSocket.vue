f14d978bce165a1a5d8b828bf3305fe5fff96eb4
vinnyfs89 <viniciusfesil@gmail.com>
2018-09-10 02:41:02 -0300

    [ADD] Criação do componente WebSocket.vue e adição do plugin vuetify

---
+++ webapp/src/views/WebSocket.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+            <v-layout column align-center>
+                <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+                <blockquote>
+                    <div class="hello">
+                        <h1>WebSocket</h1>
+                    </div>
+                    <footer>
+
+                    </footer>
+                </blockquote>
+            </v-layout>
+        </v-slide-y-transition>
+    </v-container>
+
+
+</template>
+
+<script>
+    export default {
+        name: "WebSocket",
+        updated() {
+            var conn = new WebSocket('ws://localhost:8001');
+
+            conn.onopen = function (e) {
+                console.log("Conexão estabelecida " + e);
+            }
+
+            conn.onmessage = function (e) {
+                console.log(e.data);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
