a28c8b99ad8e3163ca34d0c4d264fe625e55eee7
vinnyfs89 <viniciusfesil@gmail.com>
2018-09-12 16:50:38 -0300

    [UPDATE] Adição de Listagem das plataformas
    Co-authored-by: Ruan Nawe <ruannawe@gmail.com>

--- webapp/src/views/Plataforma.vue
+++ webapp/src/views/Plataforma.vue
@@ -9,65 +9,30 @@
 
                 <v-expansion-panel popout>
                     <v-expansion-panel-content
-                            v-for="(message, i) in messages"
+                            v-for="(plataforma, i) in plataformas"
                             :key="i"
-                            hide-actions
-                    >
+                            hide-actions>
                         <v-layout
                                 slot="header"
                                 align-center
                                 row
-                                spacer
-                        >
-                            <v-flex xs4 sm2 md1>
-                                <v-avatar
-                                        slot="activator"
-                                        size="36px"
-                                >
-                                    <img
-                                            v-if="message.avatar"
-                                            :src="message.avatar"
-                                            alt="Avatar"
-                                    >
-                                    <v-icon
-                                            v-else
-                                            :color="message.color"
-                                            v-text="message.icon"
-                                    ></v-icon>
-                                </v-avatar>
-                            </v-flex>
+                                spacer>
 
-                            <v-flex sm5 md3 hidden-xs-only>
-                                <strong v-html="message.name"></strong>
-                                <span
-                                        v-if="message.total"
-                                        class="grey--text"
-                                >
-              &nbsp;({{ message.total }})
-            </span>
+                            <v-flex sm7 md4 hidden-xs-only>
+                                <strong v-html="plataforma.plataforma_id"></strong>
                             </v-flex>
 
                             <v-flex no-wrap xs5 sm3>
-                                <v-chip
-                                        v-if="message.new"
-                                        :color="`${message.color} lighten-4`"
-                                        class="ml-0"
-                                        label
-                                        small
-                                >
-                                    {{ message.new }} new
-                                </v-chip>
-                                <strong v-html="message.title"></strong>
+                                <strong v-html="plataforma.descricao"></strong>
                             </v-flex>
 
-                            <v-flex
-                                    v-if="message.excerpt"
-                                    class="grey--text"
+                            <v-flex :class="{
+                                'red--text': !plataforma.is_ativo,
+                                'blue--text': plataforma.is_ativo,
+                            }"
                                     ellipsis
-                                    hidden-sm-and-down
-                            >
-                                &mdash;
-                                {{ message.excerpt }}
+                                    hidden-sm-and-down>
+                                {{ plataforma.is_ativo ? "Ativo" : "Inativo" }}
                             </v-flex>
                         </v-layout>
 
@@ -80,77 +45,129 @@
             </v-card>
         </v-layout>
         <!--<v-card id="create">-->
-            <!--<v-speed-dial>-->
-                <!--<v-btn slot="activator"-->
-                       <!--v-model="false"-->
-                       <!--color="blue darken-2"-->
-                       <!--dark-->
-                       <!--fab>-->
-                    <!--<v-icon>account_circle</v-icon>-->
-                    <!--<v-icon>close</v-icon>-->
-                <!--</v-btn>-->
-                <!--<v-btn fab-->
-                       <!--dark-->
-                       <!--small-->
-                       <!--color="green">-->
-                    <!--<v-icon>edit</v-icon>-->
-                <!--</v-btn>-->
-                <!--<v-btn fab-->
-                       <!--dark-->
-                       <!--small-->
-                       <!--color="indigo">-->
-                    <!--<v-icon>add</v-icon>-->
-                <!--</v-btn>-->
-                <!--<v-btn fab-->
-                       <!--dark-->
-                       <!--small-->
-                       <!--color="red">-->
-                    <!--<v-icon>delete</v-icon>-->
-                <!--</v-btn>-->
-            <!--</v-speed-dial>-->
+        <!--<v-speed-dial>-->
+        <!--<v-btn slot="activator"-->
+        <!--v-model="false"-->
+        <!--color="blue darken-2"-->
+        <!--dark-->
+        <!--fab>-->
+        <!--<v-icon>account_circle</v-icon>-->
+        <!--<v-icon>close</v-icon>-->
+        <!--</v-btn>-->
+        <!--<v-btn fab-->
+        <!--dark-->
+        <!--small-->
+        <!--color="green">-->
+        <!--<v-icon>edit</v-icon>-->
+        <!--</v-btn>-->
+        <!--<v-btn fab-->
+        <!--dark-->
+        <!--small-->
+        <!--color="indigo">-->
+        <!--<v-icon>add</v-icon>-->
+        <!--</v-btn>-->
+        <!--<v-btn fab-->
+        <!--dark-->
+        <!--small-->
+        <!--color="red">-->
+        <!--<v-icon>delete</v-icon>-->
+        <!--</v-btn>-->
+        <!--</v-speed-dial>-->
         <!--</v-card>-->
         <!--<v-btn-->
-                <!--absolute-->
-                <!--dark-->
-                <!--fab-->
-                <!--botto-->
-                <!--right-->
-                <!--color="pink"-->
+        <!--absolute-->
+        <!--dark-->
+        <!--fab-->
+        <!--botto-->
+        <!--right-->
+        <!--color="pink"-->
         <!--&gt;-->
-            <!--<v-icon>add</v-icon>-->
+        <!--<v-icon>add</v-icon>-->
         <!--</v-btn>-->
     </v-container>
 
 </template>
 <script>
+
+    import axios from 'axios'
+
     export default {
         data: () => ({
             bottomNav: 'recent',
-            messages: [
-                {
-                    avatar: 'https://cdn.icon-icons.com/icons2/936/PNG/512/mobile-phone_icon-icons.com_73484.png',
-                    name: 'Celular',
-                    title: 'Celular Título',
-                    excerpt: 'Celular descrição'
-                },
-                {
-                    avatar: 'https://image.flaticon.com/icons/svg/49/49918.svg',
-                    name: 'Web',
-                    title: 'Web Título',
-                    excerpt: 'Web descrição'
-                },
-                {
-                    color: 'red',
-                    icon: 'people',
-                    name: 'Email',
-                    new: 1,
-                    total: 3,
-                    title: 'Email Título'
-                },
+            plataformas: [
+                // {
+                //     avatar: 'https://cdn.icon-icons.com/icons2/936/PNG/512/mobile-phone_icon-icons.com_73484.png',
+                //     name: 'Celular',
+                //     title: 'Celular Título',
+                //     excerpt: 'Celular descrição'
+                // },
+                // {
+                //     avatar: 'https://image.flaticon.com/icons/svg/49/49918.svg',
+                //     name: 'Web',
+                //     title: 'Web Título',
+                //     excerpt: 'Web descrição'
+                // },
+                // {
+                //     color: 'red',
+                //     icon: 'people',
+                //     name: 'Email',
+                //     new: 1,
+                //     total: 3,
+                //     title: 'Email Título'
+                // },
             ],
             lorem: 'Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.'
-        })
+        }),
+        created() {
+            this.buscarPlataformas()
+        },
+        methods: {
+            buscarPlataformas() {
+                axios.get('http://localhost/v1/plataforma')
+                    .then(response => {
+                        const data = response.data;
+                        this.plataformas = data.data;
+                    })
+                    // .then(response => (this.info = response.data.bpi))
+                    .catch(error => {
+                        console.log(error)
+                        this.errored = true
+                    })
+                    .finally(() => this.loading = false)
+            },
+            obterClasseAtiva() {
+
+            }
+        }
     }
+
+
+    // import axios from 'axios'
+    //
+    // export default {
+    //     data () {
+    //         return {
+    //             info: null,
+    //             loading: true,
+    //             errored: false
+    //         }
+    //     },
+    //     filters: {
+    //         currencydecimal (value) {
+    //             return value.toFixed(2)
+    //         }
+    //     },
+    //     mounted () {
+    //         axios
+    //             .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    //             .then(response => (this.info = response.data.bpi))
+    //             .catch(error => {
+    //                 console.log(error)
+    //                 this.errored = true
+    //             })
+    //             .finally(() => this.loading = false)
+    //     }
+    // }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
