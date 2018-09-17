328738500952bb2e57b9c26c9755de8ddb0c23a9
vinnyfs89 <viniciusfesil@gmail.com>
2018-09-17 20:42:22 -0300

    [UPDATE] Definindo campos para migration de tabela User e ajuste de campos da model User para que também defina dados fakes para o campo password.
    source:
    https://medium.com/tech-tajawal/jwt-authentication-for-lumen-5-6-2376fd38d454

--- api/database/migrations/2018_09_17_233639_create_user_table.php
+++ api/database/migrations/2018_09_17_233639_create_user_table.php
@@ -13,8 +13,11 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
