0aedd241729b8991bbe4e56be7e02b97196f5582
vinnyfs89 <viniciusfesil@gmail.com>
2018-09-17 22:47:38 -0300

    [UPDATE] Atualizando registro de provedores de comandos para artisan.

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -13,8 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment() !== 'production') {
+        if($this->app->environment() == 'local') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(\Wn\Generators\CommandsServiceProvider::class);
         }
     }
 }
