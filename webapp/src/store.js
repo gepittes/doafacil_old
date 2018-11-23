0185f64c709a4a05fce2aa515ba207a15ac3ddbc
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-23 18:28:18 -0200

    [UPDATE] Inclusão de módulo Sistema no Store da aplicação

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -1,20 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import plataforma from '@/modules/plataforma';
 import account from '@/modules/account';
 import user from '@/modules/user';
 import alert from '@/modules/alert';
+import plataforma from '@/modules/plataforma';
+import sistema from '@/modules/sistema';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 
     modules: {
-        plataforma,
         account,
         user,
-        alert
+        alert,
+        plataforma,
+        sistema
     },
     strict: true
 });
