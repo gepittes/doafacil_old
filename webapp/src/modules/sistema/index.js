a8e016d31d9d123f244dfcb4be53af10bcbbc391
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-23 18:27:24 -0200

    [ADD] Divisão de de state para arquivo a parte

--- webapp/src/modules/sistema/index.js
+++ webapp/src/modules/sistema/index.js
@@ -1,6 +1,7 @@
 import * as actions from './actions';
 import * as getters from './getters';
-import { mutations, state } from "./mutations";
+import {mutations} from "./mutations";
+import state from "./state";
 
 export default {
     namespaced: true,
