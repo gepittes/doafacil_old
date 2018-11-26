a2fc41b3ddb7fb907932baee3e8e9cc73edde2f0
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-26 10:59:22 -0200

    [UPDATE] Definição de rota, mapeamento de componente Mensagem e substituição da View 'About' para 'Sobre'

---
+++ webapp/src/views/Sobre.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-card color="" >
+      <v-toolbar dark color="success">
+        <v-toolbar-title>Sobre!</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-layout justify-space-around>
+          <v-flex xs5>
+            <v-layout column>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </v-container>
+
+</template>
