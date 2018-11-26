d63749c2c9e637a51513fc0c66ea00cb1ab86016
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-26 19:04:19 -0200

    [ADD] Criação de módulo Conta e mapeamento do módulo na store

---
+++ webapp/src/modules/conta/actions.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import * as types from './types';
+
+export const obterContas = ({commit}) => {
+    axios.get('http://localhost/v1/conta')
+        .then(response => {
+            const data = response.data;
+            commit(types.SET_CONTA, data.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+export const removerConta = ({commit}, usuario_id) => {
+    axios.delete('http://localhost/v1/conta/' + usuario_id)
+        .then(function () {
+            commit(types.DELETE_CONTA, usuario_id);
+        });
+}
+
+export const cadastrarConta = ({commit}, conta) => {
+    return axios.post('http://localhost/v1/conta', conta)
+        .then((response) => {
+            const data = response.data;
+            commit(types.SET_CONTA, data.data)
+        });
+};
+
+export const atualizarConta = ({commit}, conta) => {
+    return axios.patch('http://localhost/v1/conta/' + conta.usuario_id, conta)
+        .then(() => {
+            commit(types.ATUALIZAR_CONTA, conta)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+};
