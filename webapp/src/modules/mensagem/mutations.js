7cf97ca04d45ab79d0d4397a5d939ffa7881ce6c
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-07 15:53:05 -0200

    [REFACTOR] Modificação de `mensagem` para `mensagens`

--- webapp/src/modules/mensagem/mutations.js
+++ webapp/src/modules/mensagem/mutations.js
@@ -1,18 +1,18 @@
 import * as types from './types';
 
 export const mutations = {
-    [types.DEFINIR_MENSAGENS](state, mensagem) {
-        state.mensagem = mensagem
+    [types.DEFINIR_MENSAGENS](state, mensagens) {
+        state.mensagens = mensagens
     },
     [types.ACRESCENTAR_MENSAGEM](state, mensagem) {
-        state.mensagem.push(mensagem);
+        state.mensagens.push(mensagem);
     },
     [types.REMOVER_MENSAGEM](state, mensagem_id) {
-        var index = state.mensagem.findIndex(mensagem => mensagem.mensagem_id === mensagem_id);
-        state.mensagem.splice(index, 1);
+        var index = state.mensagens.findIndex(mensagem => mensagem.mensagem_id === mensagem_id);
+        state.mensagens.splice(index, 1);
     },
     [types.ATUALIZAR_MENSAGEM](state, mensagem_editada) {
-        var index = state.mensagem.findIndex(mensagem => mensagem.mensagem_id === mensagem_editada.mensagem_id);
-        Object.assign(state.mensagem[index], mensagem_editada)
+        var index = state.mensagens.findIndex(mensagem => mensagem.mensagem_id === mensagem_editada.mensagem_id);
+        Object.assign(state.mensagens[index], mensagem_editada)
     },
 }
