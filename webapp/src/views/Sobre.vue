f7da01071475c08e04203fecd9698a81173d4a83
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-12 17:03:09 -0200

    [UPDATE] Utilização de lib para exibição de markdown.

--- webapp/src/views/Sobre.vue
+++ webapp/src/views/Sobre.vue
@@ -1,42 +1,63 @@
 <template>
-  <v-container fluid>
-    <v-card color="" >
-      <v-toolbar dark color="primary">
-        <v-toolbar-title>Sobre!</v-toolbar-title>
-      </v-toolbar>
-      <v-card-text>
-        <v-layout justify-space-around>
-          <v-flex xs5>
-            <v-layout column>
-            </v-layout>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-  </v-container>
+    <v-container fluid>
+        <v-card color="">
+            <v-toolbar dark color="primary">
+                <v-toolbar-title>Sobre!</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+                <v-layout justify-space-around>
+                    <v-flex xs5>
+                        <v-layout column>
+
+<!-- -->
+                            <div id="preview">
+                                <h2 class="text-success text-center">Preview</h2>
+                                <div class="well well-sm pre-scrollable" v-html='previewText'></div>
+                            </div>
+<!-- -->
+
+                        </v-layout>
+                    </v-flex>
+                </v-layout>
+            </v-card-text>
+        </v-card>
+    </v-container>
 
 </template>
 
 <script>
 
-    // import FileReader from "./FileReader";
-    //
-    // export default {
-    //     name: "app",
-    //     data: () => ({ text: "" }),
-    //     components: {
-    //         FileReader
-    //     },
-    //     methods:{
-    //
-    //     }
-    // };
-    //
-    // const reader = new FileReader();
-    //
-    // reader.onload = e => this.$emit("load", e.target.result);
-    // reader.readAsText(file);
-    //
-    // console.log(read.file('../../README.md'));
+    let marked = require('marked');
+
+    import axios from 'axios';
+
+    export default {
+        name: 'app',
+        props: ['src'],
+        data() {
+            return {
+                md_text: '# Title 2',
+            }
+        },
+        mounted() {
+            console.log(axios.get(this.src).then(response => {this.md_text = response.data}))
+        },
+        computed: {
+            previewText() {
+                marked.setOptions({
+                    renderer: new marked.Renderer(),
+                    gfm: true,
+                    tables: true,
+                    breaks: true,
+                    pedantic: false,
+                    sanitize: true,
+                    smartLists: true,
+                    smartypants: false
+                });
+                return marked(this.md_text)
+            }
+        },
+    }
+
 
 </script>
