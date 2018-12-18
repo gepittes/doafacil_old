93909f83af3156ef2e7df72d3dc6564a438455c1
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-18 18:07:32 -0200

    [FIX] Correção de definição de status como logado.

--- webapp/src/modules/account/state.js
+++ webapp/src/modules/account/state.js
@@ -2,9 +2,10 @@ import jwt_decode from "jwt-decode";
 
 const user = JSON.parse(localStorage.getItem('user'));
 const accountInfo = user != null ? jwt_decode(user) : '';
+const loggedIn = user != null ? true : false;
 
 export const state = {
-    status: { loggedIn: true },
+    status: { loggedIn: loggedIn },
     user: user ? user : '',
     accountInfo: accountInfo ? accountInfo.user : ''
 }
