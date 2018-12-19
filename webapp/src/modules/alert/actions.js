ea5828df57a102f3003c9666ab31405593dcee55
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-19 18:58:17 -0200

    [UPDATE] Tratamento de actions para exibição de mensagens com duração.

--- webapp/src/modules/alert/actions.js
+++ webapp/src/modules/alert/actions.js
@@ -1,11 +1,15 @@
-import * as types from './types'
+import * as types from './types';
 
-export const success = ({commit}, message) => {
-    commit(types.SUCCESS, message);
-}
-export const error = ({commit}, message) => {
-    commit(types.ERROR, message);
-}
-export const clear = ({commit}, message) => {
-    commit(types.CLEAR, message);
-}
+const segundosDuracaoMensagem = 8;
+
+export const success = ({ dispatch, commit }, message) => {
+  commit(types.SUCCESS, message);
+  setTimeout(() => (dispatch('alert/clear', message, { root: true })), segundosDuracaoMensagem * 1000);
+};
+export const error = ({ dispatch, commit }, message) => {
+  commit(types.ERROR, message);
+  setTimeout(() => (dispatch('alert/clear', message, { root: true })), segundosDuracaoMensagem * 1000);
+};
+export const clear = ({ commit }, message) => {
+  commit(types.CLEAR, message);
+};
