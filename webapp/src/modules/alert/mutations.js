b87b212a6c53bbc6cbaef10eaecd1614c6364fa3
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-19 16:42:39 -0200

    [REFACTOR] Adaptando arquivos aos padrões definidos do ESLint.

--- webapp/src/modules/alert/mutations.js
+++ webapp/src/modules/alert/mutations.js
@@ -1,19 +1,19 @@
 import * as types from './types';
 
 export default {
-    [types.GETALLREQUEST](state) {
-        state.all = { loading: true };
-    },
-    [types.SUCCESS](state, message) {
-        state.message_type = 'alert-success';
-        state.message = message;
-    },
-    [types.ERROR](state, message) {
-        state.message_type = 'alert-danger';
-        state.message = message;
-    },
-    [types.CLEAR](state) {
-        state.message_type = null;
-        state.message = null;
-    }
-}
+  [types.GETALLREQUEST](state) {
+    state.all = { loading: true };
+  },
+  [types.SUCCESS](state, message) {
+    state.message_type = 'alert-success';
+    state.message = message;
+  },
+  [types.ERROR](state, message) {
+    state.message_type = 'alert-danger';
+    state.message = message;
+  },
+  [types.CLEAR](state) {
+    state.message_type = null;
+    state.message = null;
+  },
+};
