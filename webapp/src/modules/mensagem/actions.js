e3ce48d3bb72f8f9e3cef8c3b6bb837a05d6baa3
vinnyfs89 <viniciusfesil@gmail.com>
2018-12-26 18:43:53 -0200

    [REFACTOR] Adaptando arquivos aos padrões definidos do ESLint.

--- webapp/src/modules/mensagem/actions.js
+++ webapp/src/modules/mensagem/actions.js
@@ -1,38 +1,36 @@
 import axios from 'axios';
 import * as types from './types';
 
-export const obterMensagems = ({commit}) => {
-    axios.get('http://localhost/v1/mensagem')
-        .then(response => {
-            const data = response.data;
-            commit(types.DEFINIR_MENSAGENS, data.data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
-
-export const removerMensagem = ({commit}, mensagem_id) => {
-    axios.delete('http://localhost/v1/mensagem/' + mensagem_id)
-        .then(function () {
-            commit(types.REMOVER_MENSAGEM, mensagem_id);
-        });
-}
-
-export const cadastrarMensagem = ({commit}, mensagem) => {
-    return axios.post('http://localhost/v1/mensagem', mensagem)
-        .then((response) => {
-            const data = response.data;
-            commit(types.ACRESCENTAR_MENSAGEM, data.data)
-        });
+export const obterMensagems = ({ commit }) => {
+  axios.get('http://localhost/v1/mensagem')
+    .then((response) => {
+      const data = response.data;
+      commit(types.DEFINIR_MENSAGENS, data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-export const atualizarMensagem = ({commit}, mensagem) => {
-    return axios.patch('http://localhost/v1/mensagem/' + mensagem.mensagem_id, mensagem)
-        .then(() => {
-            commit(types.ATUALIZAR_MENSAGEM, mensagem)
-        })
-        .catch(error => {
-            console.log(error);
-        })
+export const removerMensagem = ({ commit }, mensagem_id) => {
+  axios.delete(`http://localhost/v1/mensagem/${mensagem_id}`)
+    .then(() => {
+      commit(types.REMOVER_MENSAGEM, mensagem_id);
+    });
 };
+
+export const cadastrarMensagem = ({ commit }, mensagem) => axios.post('http://localhost/v1/mensagem', mensagem)
+  .then((response) => {
+    const data = response.data;
+    commit(types.ACRESCENTAR_MENSAGEM, data.data);
+  }).catch((error) => {
+    console.log(error);
+  });
+
+export const atualizarMensagem = ({ commit }, mensagem) => axios.patch(`http://localhost/v1/mensagem/${mensagem.mensagem_id}`, mensagem)
+  .then(() => {
+    commit(types.ATUALIZAR_MENSAGEM, mensagem);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
