8da530ffac31a80116ee5952100253a880741e13
vinnyfs89 <viniciusfesil@gmail.com>
2019-01-18 19:13:17 -0200

    [UPDATE] Criação de migration UsuarioSistemaTable

---
+++ api/database/migrations/2019_01_18_210412_usuario_sistema_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsuarioSistemaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notificacao.usuario_has_sistema', function(Blueprint $table) {
+            $table->integer('usuario_id');
+            $table->integer('sistema_id');
+        });
+
+        Schema::table('notificacao.usuario_has_sistema', function (Blueprint $table) {
+            $table->foreign('usuario_id')->references('usuario_id')->on('notificacao.usuario');
+        });
+
+        Schema::table('notificacao.usuario_has_sistema', function (Blueprint $table) {
+            $table->foreign('sistema_id')->references('sistema_id')->on('notificacao.sistema');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notificacao.usuario_has_sistema');
+    }
+}
