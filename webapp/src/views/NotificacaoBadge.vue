66a6a4a6f9bb88cc845f895176574332f670eb6b
vinnyfs89 <viniciusfesil@gmail.com>
2019-01-23 15:16:48 -0200

    [ADD] Adição do componente NotificacaoBadge

---
+++ webapp/src/views/NotificacaoBadge.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-badge left>
+        <span slot="badge">6</span>
+        <v-icon
+                large
+                color="grey lighten-1"
+        >
+            shopping_cart
+        </v-icon>
+    </v-badge>
+</template>
+
+<script>
+export default {
+  name: 'NotificacaoBadge',
+};
+</script>
+
+<style scoped>
+
+</style>
