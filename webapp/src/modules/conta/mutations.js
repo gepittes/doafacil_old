f501d6fbb93f70a28e21486f8d88a84cbba53c78
vinnyfs89 <viniciusfesil@gmail.com>
2019-02-27 16:49:25 -0300

    [REFACTOR] WEBAPP/CONTA : Ajustes de código para padrão eslint #117
    Co-authored-by: Guilheme Amado <amado.silva133@gmail.com>
    Co-authored-by: Enzo Alexsander <enzoalexsander9@gmail.com>

--- webapp/src/modules/conta/mutations.js
+++ webapp/src/modules/conta/mutations.js
@@ -7,12 +7,12 @@ export const mutations = {
     [types.ACRESCENTAR_CONTA](state, conta) {
         state.conta.push(conta);
     },
-    [types.DELETE_CONTA](state, usuario_id) {
-        const index = state.conta.findIndex(conta => conta.usuario_id === usuario_id);
+    [types.DELETE_CONTA](state, usuarioId) {
+        const index = state.conta.findIndex(conta => conta.usuario_id === usuarioId);
         state.conta.splice(index, 1);
     },
-    [types.ATUALIZAR_CONTA](state, conta_editada) {
-        const index = state.conta.findIndex(conta => conta.usuario_id === conta_editada.usuario_id);
-        Object.assign(state.conta[index], conta_editada);
+    [types.ATUALIZAR_CONTA](state, contaEditada) {
+        const index = state.conta.findIndex(conta => conta.usuario_id === contaEditada.usuario_id);
+        Object.assign(state.conta[index], contaEditada);
     },
 };
