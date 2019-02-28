387d83a000d714a74d4f8e8cd7fb0a4a208a9136
vinnyfs89 <viniciusfesil@gmail.com>
2019-02-28 12:17:15 -0300

    [REFACTOR] WEBAPP/SISTEMA : Ajustes para que esteja de acordo com  definições do ESLint. #72

--- webapp/src/modules/sistema/SistemaFormulario.vue
+++ webapp/src/modules/sistema/SistemaFormulario.vue
@@ -2,41 +2,42 @@
     <v-container grid-list-md>
         <v-layout wrap>
             <v-flex
-                    xs12
-                    sm6
-                    md12>
+                xs12
+                sm6
+                md12>
                 <v-text-field
-                        v-model="editedItem.descricao"
-                        required
-                        label="Descrição"/>
+                    v-model="editedItem.descricao"
+                    required
+                    label="Descrição"/>
             </v-flex>
             <v-flex
-                    xs12
-                    sm6
-                    md12>
+                xs12
+                sm6
+                md12>
                 <v-text-field
-                        v-model="editedItem.url"
-                        required
-                        label="URL"/>
+                    v-model="editedItem.url"
+                    required
+                    label="URL"/>
             </v-flex>
             <v-flex
-                    xs12
-                    sm6
-                    md12>
+                xs12
+                sm6
+                md12>
                 <h3>Situação</h3>
                 <v-switch
-                        :label="`${editedItem.is_ativo ? 'Ativo' : 'Inativo'}`"
-                        v-model="editedItem.is_ativo"/>
+                    :label="`${editedItem.is_ativo ? 'Ativo' : 'Inativo'}`"
+                    v-model="editedItem.is_ativo"/>
             </v-flex>
             <v-flex class="text-xs-center">
                 <v-btn
-                        color="error"
-                        dark
-                        @click.native="close">Fechar</v-btn>
-                <v-btn v-if="!loading"
-                       dark
-                       color="blue darken-1"
-                       @click.native="save">Gravar
+                    color="error"
+                    dark
+                    @click.native="close">Fechar</v-btn>
+                <v-btn
+                    v-if="!loading"
+                    dark
+                    color="blue darken-1"
+                    @click.native="save">Gravar
                 </v-btn>
             </v-flex>
         </v-layout>
@@ -45,67 +46,67 @@
 </template>
 <script>
 
-  import {mapActions} from 'vuex';
+import { mapActions } from 'vuex';
 
-  export default {
+export default {
     props: {
-      item: {
-        type: Object,
-        default: () => {
+        item: {
+            type: Object,
+            default: () => {
+            },
         },
-      },
-      dialog: {
-        type: Boolean,
-        default: false,
-        required: false,
-      },
+        dialog: {
+            type: Boolean,
+            default: false,
+            required: false,
+        },
     },
     data: () => ({
-      loading: false,
-      editedItem: {},
-      defaultItem: {
-        sistema_id: null,
-        descricao: '',
-        url: '',
-        is_ativo: true,
-      },
+        loading: false,
+        editedItem: {},
+        defaultItem: {
+            sistema_id: null,
+            descricao: '',
+            url: '',
+            is_ativo: true,
+        },
     }),
 
-    mounted() {
-      this.editedItem = Object.assign({}, this.defaultItem);
+    watch: {
+        item(value) {
+            this.editedItem = Object.assign({}, value);
+        },
     },
 
-    watch: {
-      item(value) {
-        this.editedItem = Object.assign({}, value);
-      },
+    mounted() {
+        this.editedItem = Object.assign({}, this.defaultItem);
     },
 
     methods: {
 
-      ...mapActions({
-        cadastrarSistema: 'sistema/cadastrarSistema',
-        atualizarSistema: 'sistema/atualizarSistema',
-      }),
+        ...mapActions({
+            cadastrarSistema: 'sistema/cadastrarSistema',
+            atualizarSistema: 'sistema/atualizarSistema',
+        }),
 
-      save() {
-        const self = this;
-        if (self.editedItem.is_ativo === null || self.editedItem.is_ativo === '') {
-          self.editedItem.is_ativo = false;
-        }
-        if (self.editedItem.sistema_id !== null) {
-          self.atualizarSistema(self.editedItem);
-        } else {
-          self.cadastrarSistema(self.editedItem);
-        }
-        this.$emit('update:dialog', false);
-      },
+        save() {
+            const self = this;
+            if (self.editedItem.is_ativo === null || self.editedItem.is_ativo === '') {
+                self.editedItem.is_ativo = false;
+            }
+            if (self.editedItem.sistema_id !== null) {
+                self.atualizarSistema(self.editedItem);
+            } else {
+                self.cadastrarSistema(self.editedItem);
+            }
+            this.$emit('update:dialog', false);
+        },
 
-      close() {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.$emit('update:dialog', false);
-      },
+        close() {
+            this.editedItem = Object.assign({}, this.defaultItem);
+            this.$emit('update:dialog', false);
+        },
     },
-  };
+};
 
 </script>
