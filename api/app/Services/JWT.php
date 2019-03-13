6159901617755a205e9679e1e59c686a9bee5636
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-13 11:51:11 -0300

    [ADD] Criação de serviço específico para geração de tokens JWT. #116 #88

---
+++ api/app/Services/JWT.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Usuario;
+use Validator;
+
+/**
+ * Class JWT
+ *
+ * @package App\Services
+ */
+class JWT implements IService
+{
+    private $_emissorToken = "lumen-jwt";
+    private $_assuntoToken;
+    private $_horarioEmissaoToken;
+    private $_tempoExpiracaoToken;
+
+    public function criarToken(Usuario $usuario) : array
+    {
+        $this->_assuntoToken = $usuario->usuario_id;
+        $this->_horarioEmissaoToken = time();
+        $this->_tempoExpiracaoToken = $this->_horarioEmissaoToken + 60 * 60;
+
+        $payload = [
+            'iss' => $this->_emissorToken,
+            'sub' => $this->_assuntoToken,
+            'user' => [
+                'user_id' => $usuario->usuario_id,
+                'name' => $usuario->nome,
+                'email' => $usuario->email,
+                'sistemas' => $usuario->sistemas,
+                'is_admin' => $usuario->is_admin,
+            ],
+            'iat' => $this->_horarioEmissaoToken,
+            'exp' => $this->_tempoExpiracaoToken
+        ];
+
+        if (!env('JWT_SECRET')) {
+            throw new \Exception(
+                'Vari&aacute;vel de ambiente `JWT_SECRET` n&atilde;o definida.'
+            );
+        }
+
+        return [
+            'token' => JWT::encode($payload, env('JWT_SECRET'))
+        ];
+    }
+}
