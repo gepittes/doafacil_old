88f457218f5b7bb1131b3d19ff40b89d08fd4382
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-13 17:30:29 -0300

    [UPDATE] Adição de dependência que trás comandos que podem ser utilizado no artisan.

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -101,6 +101,7 @@ $app->routeMiddleware([
 */
 
 $app->register(App\Providers\AppServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
