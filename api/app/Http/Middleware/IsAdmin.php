0c1a97c57312b9fb9a4daec21a46f0d7ebe749bc
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-15 15:54:12 -0300

    [UPDATE] Ajustes no Middleware `IsAdmin` para interromper a resposta quando o usuário não possuir token.
    #143 #116 #88

--- api/app/Http/Middleware/IsAdmin.php
+++ api/app/Http/Middleware/IsAdmin.php
@@ -9,14 +9,20 @@ class IsAdmin
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        $token = $request->bearerToken();
+        if (!$token) {
+            abort(403, 'Acesso negado.');
+        }
 
+        // antes do middleware
         $response = $next($request);
+        // depois do middleware
 
         return $response;
     }
