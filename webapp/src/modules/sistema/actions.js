32adec9f68384d2ad99cdd98f0063b5f7cd64e3a
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-18 15:23:40 -0300

    [UPDATE] Utilização do helper **requisicaoAutorizada** para todas as requisições que exigem token.
    #143 #116 #88

--- webapp/src/modules/sistema/actions.js
+++ webapp/src/modules/sistema/actions.js
@@ -1,48 +1,41 @@
-import axios from 'axios';
 import * as types from './types';
+import { requisicaoAutorizada } from '../account/_helpers/requisicao-autorizada';
 
 export const obterSistemas = ({ dispatch, commit }) => {
-    axios.get('http://localhost/v1/sistema')
-        .then((response) => {
-            const { data } = response.data;
-            commit(types.OBTER_SISTEMAS, data);
-        })
-        .catch((error) => {
-            dispatch('alert/error', error.response.data.error, {
-                root: true,
-            });
-        });
-};
-
-export const removerSistema = ({ dispatch, commit }, sistemaId) => {
-    axios.delete(`http://localhost/v1/sistema/${sistemaId}`)
-        .then(() => {
-            commit(types.DELETE_SISTEMA, sistemaId);
-        })
-        .catch((error) => {
-            dispatch('alert/error', error.response.data.error, {
-                root: true,
-            });
-        });
-};
-
-export const cadastrarSistema = ({ dispatch, commit }, sistema) => axios.post('http://localhost/v1/sistema', sistema)
-    .then((response) => {
+    requisicaoAutorizada.get('http://localhost/v1/sistema').then((response) => {
         const { data } = response.data;
-        commit(types.ACRESCENTAR_SISTEMA, data.data);
-        dispatch('alert/success', 'Cadastro realizado com sucesso!', { root: true });
+        commit(types.OBTER_SISTEMAS, data);
     }).catch((error) => {
         dispatch('alert/error', error.response.data.error, {
             root: true,
         });
     });
+};
 
-export const atualizarSistema = ({ dispatch, commit }, sistema) => axios.patch(`http://localhost/v1/sistema/${sistema.sistema_id}`, sistema)
-    .then(() => {
-        commit(types.ATUALIZAR_SISTEMA, sistema);
-    })
-    .catch((error) => {
+export const removerSistema = ({ dispatch, commit }, sistemaId) => {
+    requisicaoAutorizada.delete(`http://localhost/v1/sistema/${sistemaId}`).then(() => {
+        commit(types.DELETE_SISTEMA, sistemaId);
+    }).catch((error) => {
         dispatch('alert/error', error.response.data.error, {
             root: true,
         });
     });
+};
+
+export const cadastrarSistema = ({ dispatch, commit }, sistema) => requisicaoAutorizada.post('http://localhost/v1/sistema', sistema).then((response) => {
+    const { data } = response.data;
+    commit(types.ACRESCENTAR_SISTEMA, data.data);
+    dispatch('alert/success', 'Cadastro realizado com sucesso!', { root: true });
+}).catch((error) => {
+    dispatch('alert/error', error.response.data.error, {
+        root: true,
+    });
+});
+
+export const atualizarSistema = ({ dispatch, commit }, sistema) => requisicaoAutorizada.patch(`http://localhost/v1/sistema/${sistema.sistema_id}`, sistema).then(() => {
+    commit(types.ATUALIZAR_SISTEMA, sistema);
+}).catch((error) => {
+    dispatch('alert/error', error.response.data.error, {
+        root: true,
+    });
+});
