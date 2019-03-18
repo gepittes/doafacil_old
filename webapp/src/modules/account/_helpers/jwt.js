c1d27fb1f19f25833897e9d2a7f8aa63776dda05
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-18 13:11:30 -0300

    [UPDATE] Adição de validação para quando a variável de ambiente **VUE_APP_JWT_SECRET** não for definida.

--- webapp/src/modules/account/_helpers/jwt.js
+++ webapp/src/modules/account/_helpers/jwt.js
@@ -2,6 +2,11 @@ import * as jsonwebtoken from 'jsonwebtoken';
 
 export function obterInformacoesJWT() {
     try {
+        if (process.env.VUE_APP_JWT_SECRET == null) {
+            const error = 'Variável de ambiente `VUE_APP_JWT_SECRET` não definida no arquivo `.env`.';
+            throw error;
+        }
+
         const token = localStorage.getItem('token');
         return token != null ? jsonwebtoken.verify(token, process.env.VUE_APP_JWT_SECRET) : '';
     } catch (Exception) {
