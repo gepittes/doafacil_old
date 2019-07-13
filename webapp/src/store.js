f23882251471d78ff902517f7b3e92192fa43cd6
Jadilson Guedes <jadilson12@gmail.com>
2019-07-13 13:37:43 -0300

    [ADD] Adicionando o modules instituição na store

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -5,6 +5,7 @@ import account from '@/modules/account';
 import user from '@/modules/user';
 import alert from '@/modules/alert';
 import conta from '@/modules/conta';
+import instituicao from '@/modules/instituicao';
 
 
 Vue.use(Vuex);
@@ -16,6 +17,7 @@ export default new Vuex.Store({
         user,
         alert,
         conta,
+        instituicao,
     },
     strict: true,
 });
