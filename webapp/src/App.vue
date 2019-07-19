b4789bd4d334f89a88249ad639224b6d777ff527
gabriel-roque <gabrielrms.dll@gmail.com>
2019-07-19 14:17:36 -0300

    [ADD] Adicionando Logo DoaFacil

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -2,22 +2,28 @@
     <v-app>
         <barra-lateral v-model="drawer"/>
         <v-toolbar
-            :clipped-left="clipped"
-            app
-            dark
-            color="primary">
-            <v-toolbar-side-icon
-                v-if="status.loggedIn"
-                @click.stop="drawer = !drawer"/>
-            <v-toolbar-title v-text="title"/>
+                :clipped-left="clipped"
+                app
+                dark
+                color="#00b5dd">
 
+            <v-toolbar-side-icon
+                    v-if="status.loggedIn"
+                    @click.stop="drawer = !drawer"/>
+            <v-toolbar-title/>
+            <v-img
+                    :max-width="100"
+                    :src="logo"
+                    :color="white"
+            />
         </v-toolbar>
 
         <v-content>
             <alerta
-                v-if="alert.message != null && alert.message_type != null"
-                :color="alert.message_type"
-            >{{ alert.message }}</alerta>
+                    v-if="alert.message != null && alert.message_type != null"
+                    :color="alert.message_type"
+            >{{ alert.message }}
+            </alerta>
             <router-view/>
         </v-content>
 
@@ -26,65 +32,65 @@
 
 <script>
 
-import { mapState, mapActions, mapGetters } from 'vuex';
-import BarraLateral from './modules/core/BarraLateral.vue';
-import Alerta from './modules/alert/Alerta.vue';
+    import {mapState, mapActions, mapGetters} from 'vuex';
+    import BarraLateral from './modules/core/BarraLateral.vue';
+    import Alerta from './modules/alert/Alerta.vue';
 
-export default {
-    name: 'App',
-    components: {
-        BarraLateral,
-        Alerta,
-    },
-    data() {
-        return {
-            loading: false,
-            clipped: false,
-            drawer: false,
-            fixed: false,
-            items: [
-                {
-                    icon: 'home',
-                    title: 'Início',
-                    to: '/',
-                },
-                {
-                    icon: 'home',
-                    title: 'Instituições',
-                    to: '/instituicoes',
-                },
-                {
-                    icon: 'edit',
-                    title: 'Administração',
-                    to: '/administracao',
-                },
+    export default {
+        name: 'App',
+        components: {
+            BarraLateral,
+            Alerta,
+        },
+        data() {
+            return {
+                loading: false,
+                clipped: false,
+                drawer: false,
+                fixed: false,
+                items: [
+                    {
+                        icon: 'home',
+                        title: 'Início',
+                        to: '/',
+                    },
+                    {
+                        icon: 'home',
+                        title: 'Instituições',
+                        to: '/instituicoes',
+                    },
+                    {
+                        icon: 'edit',
+                        title: 'Administração',
+                        to: '/administracao',
+                    },
 
-            ],
-            miniVariant: false,
-            right: true,
-            rightDrawer: false,
-            title: 'DoaFácil',
-        };
-    },
-    computed: {
-        ...mapState({
-            alert: state => state.alert,
-            // isLoggedIn: state => state.isLoggedIn
-            // status: state => state.status
-        }),
-        ...mapGetters({
-            status: 'account/status',
-            user: 'account/user',
-            accountInfo: 'account/accountInfo',
-        }),
-    },
-    mounted() {
-        this.loading = true;
-    },
-    methods: {
-        ...mapActions({
-            clearAlert: 'alert/clear',
-        }),
-    },
-};
+                ],
+                miniVariant: false,
+                right: true,
+                rightDrawer: false,
+                logo: require('../src/assets/doafacil/logo-doafacil.png')
+            };
+        },
+        computed: {
+            ...mapState({
+                alert: state => state.alert,
+                // isLoggedIn: state => state.isLoggedIn
+                // status: state => state.status
+            }),
+            ...mapGetters({
+                status: 'account/status',
+                user: 'account/user',
+                accountInfo: 'account/accountInfo',
+            }),
+        },
+        mounted() {
+            this.loading = true;
+        },
+        methods: {
+            ...mapActions({
+                clearAlert: 'alert/clear',
+            }),
+        },
+    };
 </script>
