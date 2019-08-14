317fd868e7a3a4b320e0f9d2918be471eb911c9e
gabriel-roque <gabrielrms.dll@gmail.com>
2019-08-14 08:52:28 -0300

    [REFECTOR] Reorganizando components em pastas corretas

--- webapp/src/views/home/Home.vue
+++ webapp/src/views/home/Home.vue
@@ -18,26 +18,25 @@
         <CompInfo/>
 
         <Footer/>
-
     </v-app>
 </template>
 
 <script>
-    import Fachada from '../../modules/home/FachadaHome.vue'
-    import InfoCards from '../../modules/home/InformacoesCards.vue'
-    import GenericComp from '../../modules/home/GenericComp.vue'
-    import News from '../../modules/home/NewsLetter.vue'
-    import Social from '../../modules/home/RedesSociais.vue'
-    import CompInfo from '../../modules/home/InfoCompany.vue'
-    import Footer from '../../modules/core/Footer.vue'
+    import Fachada from '@/components/home/FachadaHome.vue'
+    import InfoCards from '@/components/home/InformacoesCards.vue'
+    import GenericComp from '@/components/home/GenericComp.vue'
+    import News from '@/components/home/NewsLetter.vue'
+    import Social from '@/components/home/RedesSociais.vue'
+    import CompInfo from '@/components/home/InfoCompany.vue'
+    import Footer from '@/modules/core/Footer.vue'
 
     export default {
         name: "Home",
         data: function () {
             return {
                 images: {
-                    main: require('../../assets/home/union-hands.jpg'),
-                    logo: require('../../assets/doafacil/logos/logo-doafacil.png'),
+                    main: require('@/assets/home/union-hands.jpg'),
+                    logo: require('@/assets/doafacil/logos/logo-doafacil.png'),
                 },
             };
         },
