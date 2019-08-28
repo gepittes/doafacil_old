33e78992b6e4cca430e57c77be1063cd5dfd5035
Jadilson Guedes <jadilson12@gmail.com>
2019-08-28 10:11:23 -0300

    [UPDATE] Atualiza listar instituições

--- webapp/src/components/instituicao/Instituicoes.vue
+++ webapp/src/components/instituicao/Instituicoes.vue
@@ -1,48 +1,60 @@
 <template>
     <v-app>
-        <v-container fluid grid-list-md>
-            <v-layout row wrap>
-                <Instituicao
-                    v-for="instituicao in instituicoesIniciais" :key="instituicao.id"
-                    :instituicao="instituicao"
-                />
-            </v-layout>
-        </v-container>
-        <v-btn
-            fab
-            color="success"
-            dark
-            fixed
-            bottom
-            right
-            @click="dialog = !dialog">
-            <v-icon>add</v-icon>
-        </v-btn>
+        <b-container class="container">
+            <div class="row">
+                <div class="col-lg-3">
+            
+                    <menu-instituicao/>
+        
+                </div>
+                <div class="col-lg-9 ">
+                    <v-layout row wrap>
+                        <Instituicao
+                            v-for="instituicao in instituicoesIniciais" 
+                            :key="instituicao.id"
+                            :instituicao="instituicao"
+                        />
+                    </v-layout>
+                </div>
+                
+            </div>
+            <v-btn
+                fab
+                color="success"
+                dark
+                fixed
+                bottom
+                right
+                @click="dialog = !dialog">
+                <v-icon>add</v-icon>
+            </v-btn>
 
-        <v-dialog
-            v-model="dialog"
-            max-width="700px">
-            <v-card light>
-                <v-card-text>
-                    <v-toolbar
-                        dark
-                        color="primary">
-                        <v-toolbar-title>Cadastrar nova Instituição</v-toolbar-title>
-                    </v-toolbar>
-                    <InstituicaoFormulario :dialog.sync="dialog"/>
-                </v-card-text>
-            </v-card>
-        </v-dialog>
+            <v-dialog
+                v-model="dialog"
+                max-width="700px">
+                <v-card light>
+                    <v-card-text>
+                        <v-toolbar
+                            dark
+                            color="primary">
+                            <v-toolbar-title>Cadastrar nova Instituição</v-toolbar-title>
+                        </v-toolbar>
+                        <InstituicaoFormulario :dialog.sync="dialog"/>
+                    </v-card-text>
+                </v-card>
+            </v-dialog>
+        </b-container>
     </v-app>
 </template>
 <script>
     import {mapActions, mapGetters} from 'vuex';
+    import MenuInstituicao from '../../modules/core/MenuInstituicao'
     import InstituicaoFormulario from './InstituicaoFormulario'
     import Perfil from './Perfil'
     import Instituicao from './Instituicao'
 
     export default {
-        components: {InstituicaoFormulario, Perfil, Instituicao},
+        components: { InstituicaoFormulario, Perfil, Instituicao, MenuInstituicao },
         data() {
             return {
                 votos: 3,
@@ -78,3 +90,15 @@
     }
 
 </script>
+<style>
+
+    .container {
+        width: 1020px;
+    }
+
+    @media (max-width: 600px) {
+        #avatar { display: none; }
+
+    }
+ 
+</style>
