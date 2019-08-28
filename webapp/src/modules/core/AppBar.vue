654d1b51f0e1a7b79fd1e24dfd25dc1e3506153b
Jadilson Guedes <jadilson12@gmail.com>
2019-08-28 10:12:32 -0300

    [UPDATE] Centaliza o conteudo da AppBar

--- webapp/src/modules/core/AppBar.vue
+++ webapp/src/modules/core/AppBar.vue
@@ -4,39 +4,42 @@
             <barra-lateral v-model="drawer"/>
         </template>
 
-        <v-app-bar :clipped-left="clipped" app dark class="gradient-doafacil-bg" grid-list-md text-xs-center>
-            <v-app-bar-nav-icon  id="menuLateral" v-if="status.loggedIn" @click="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-img :max-width="100" :src="logo"/>
-            <v-spacer></v-spacer>
-            <v-toolbar-items v-if="!status.loggedIn">
-                <v-btn @click="$vuetify.goTo('#sobre', options)" text>Quem somos</v-btn>
-                <v-btn to="/login" text>
-                    <v-icon>fa fa-sign-in-alt mr-2</v-icon>
-                    Login
-                </v-btn>
-            </v-toolbar-items>
-            <v-toolbar-items class="pa-1" v-if="status.loggedIn">
-                <v-menu offset-y>
-                    <template v-slot:activator="{ on }">
-
-                        <v-btn text v-on="on">
-                            <v-avatar>
-                                <i class="material-icons">account_circle</i>
-                            </v-avatar>
-                            <v-list-item-content text>
-                                <v-list-item-title text>{{ accountInfo.name }}</v-list-item-title>
-                            </v-list-item-content>
+        <v-app-bar :clipped-left="clipped" app dark class="gradient-doafacil-bg">
+            <v-container >
+                <v-app-bar flat class="gradient-doafacil-bg" >
+                    <v-app-bar-nav-icon  id="menuLateral" v-if="status.loggedIn" @click="drawer = !drawer"></v-app-bar-nav-icon>
+                    <v-img :max-width="100" :src="logo"/>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items v-if="!status.loggedIn">
+                        <v-btn @click="$vuetify.goTo('#sobre', options)" text>Quem somos</v-btn>
+                        <v-btn to="/login" text>
+                            <v-icon>fa fa-sign-in-alt mr-2</v-icon>
+                            Login
                         </v-btn>
-                    <!-- </v-btn> -->
+                    </v-toolbar-items>
+                    <v-toolbar-items class="pa-1" v-if="status.loggedIn">
+                        <v-menu offset-y>
+                            <template v-slot:activator="{ on }">
 
-                    </template>
-                    <v-list>
-                        <v-list-item @click.stop to="/logout">
-                            <v-list-item-title>Sair</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </v-toolbar-items>
+                                <v-btn text v-on="on">
+                                    <v-avatar>
+                                        <i class="material-icons">account_circle</i>
+                                    </v-avatar>
+                                    <v-list-item-content text>
+                                        <v-list-item-title text>{{ accountInfo.name }}</v-list-item-title>
+                                    </v-list-item-content>
+                                </v-btn>
+
+                            </template>
+                            <v-list>
+                                <v-list-item @click.stop to="/logout">
+                                    <v-list-item-title>Sair</v-list-item-title>
+                                </v-list-item>
+                            </v-list>
+                        </v-menu>
+                    </v-toolbar-items>
+                </v-app-bar>
+            </v-container>
         </v-app-bar>
     </div>
    
@@ -75,5 +78,11 @@ export default {
     @media (min-width: 600px) {
         #menuLateral { display: none; }
 
+    }
+
+    .v-toolbar__content{
+        padding-left: 0;
+        padding-right: 0;
+        /* height: 50px !important; */ 
     }
 </style>
