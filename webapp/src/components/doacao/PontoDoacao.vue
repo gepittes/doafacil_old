46fed94c374a507f572782a587daae1abde9dbe7
Jadilson Guedes <jadilson12@gmail.com>
2019-08-28 10:49:24 -0300

    [UPDATE] Atualizando template para ponto de Doação

--- webapp/src/components/doacao/PontoDoacao.vue
+++ webapp/src/components/doacao/PontoDoacao.vue
@@ -1,13 +1,29 @@
 <template>
-    <h1>Ponto de doação</h1>
+    <v-app>
+        <v-container fluid grid-list-md>
+            <div class="row">
+                <div class="col-lg-3">
+
+                    <menu-instituicao/>
+
+                </div>
+                <div class="col-lg-9 ">
+                    <v-layout row wrap>
+                            <h1>Listar Card de ponto de doação</h1>
+                    </v-layout>
+                </div>
+            </div>
+            
+        </v-container>    
+    </v-app>
 </template>
-
 <script>
+    import MenuInstituicao from '../../modules/core/MenuInstituicao'
+
     export default {
-        name: 'PontoDoacao'
-    };
+        components: { MenuInstituicao },
+        name: 'PontoDeDoacao',
+  
+    }
+
 </script>
-
-<style scoped>
-
-</style>
