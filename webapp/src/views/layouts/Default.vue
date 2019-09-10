adae605d066f473bf5a05fedf188942e6af20584
Jadilson Guedes <jadilson12@gmail.com>
2019-09-09 21:50:17 -0300

    [UPDATE] Atualizando layouts default

--- webapp/src/views/layouts/Default.vue
+++ webapp/src/views/layouts/Default.vue
@@ -1,29 +1,37 @@
 <template>
     <v-app>
         <app-bar class="mb-5"/>
-        <v-container class="mt-4">
-            <v-row justify="center">
-                <v-col cols="3" 
-                md="2"
-                class="pa-0">
-                    <!-- <v-col 
-                    class="user-pro-img"
-                    justify="center"
-                    
+        <v-container class="mt-4" grid-list-md>
+            <v-layout
+            row
+            wrap>
+                <v-row justify="center">
+                    <v-col cols="3" 
+                    md="2"
+                    lg="2"
+                    v-if="menu"
+                    class="pa-0">
+                        <!-- <v-col 
+                        class="user-pro-img"
+                        justify="center"
+                        
+                        >
+                            <img
+                                src="http://via.placeholder.com/170x170"
+                                alt="">
+                        </v-col> -->
+                        <menu-instituicao :show-avatar="false"/>
+                    </v-col>
+                    <v-col 
+                    :cols="main" 
+                    md="10"
+                    lg="10"
+                    class="pa-0 mb-2"
                     >
-                        <img
-                            src="http://via.placeholder.com/170x170"
-                            alt="">
-                    </v-col> -->
-                    <menu-instituicao :show-avatar="false"/>
-                </v-col>
-                <v-col cols="9" 
-                md="10"
-                class="pa-0 mb-2"
-                >
-                    <router-view/>
-                </v-col>
-            </v-row>
+                        <router-view/>
+                    </v-col>
+                </v-row>
+            </v-layout>
             <alerta
                 v-if="alert.message != null && alert.message_type != null"
                 :color="alert.message_type"
@@ -39,13 +47,40 @@ import AppBar from '../../modules/core/AppBar'
 import MenuInstituicao from '../../modules/core/MenuInstituicao.vue';
 import Alerta from '../../modules/alert/Alerta.vue';
 
-
 export default {
     name: 'Main',
     components: { 
         MenuInstituicao, 
         Alerta,
-        AppBar},
+        AppBar
+    },
+    data(){
+        return {
+            window: {
+                width: 0,
+            },
+            menu: true,
+            main:9
+        }
+    },
+    watch: {
+        'window.width': function (width) {
+            if (width < 700) {
+                this.menu = false
+                this.main = 12
+            }
+            if (width > 700) {
+                this.menu = true
+                this.main = 9
+            }
+            this.window.width = width;
+        },
+    },
+    created() {
+        window.addEventListener('resize', this.handleResize)
+        this.handleResize();
+    },
+
     computed: {
         options() {
             return {
@@ -68,6 +103,10 @@ export default {
         ...mapActions({
             clearAlert: 'alert/clear',
         }),
+
+        handleResize() {
+            this.window.width = window.innerWidth;
+        },
     },
     
 };
