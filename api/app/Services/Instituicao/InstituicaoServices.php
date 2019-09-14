1c812403b999752e9b7638cc6819141a8b78bfcf
Jadilson Guedes <jadilson12@gmail.com>
2019-09-14 17:42:39 -0300

    [ADD] Cria  instituicaoService com todos com os metodos simplificados

---
+++ api/app/Services/Instituicao/InstituicaoServices.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services\Instituicao;
+
+use App\Models\Instituicao;
+
+class InstituicaoServices
+{
+    public function obter($id = null, $nameFk = null)
+    {
+        $data = Instituicao::all();
+
+        try {
+            if (!empty(trim($id))) {
+                $data =  Instituicao::where('id','=',$id)->get();
+            }
+
+            if (!empty(trim($nameFk))) {
+                $data =  Instituicao::where($nameFk,'=',$id)->get();
+            }
+
+            return $data;
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function criar(array $dados = [])
+    {
+        try {
+
+            return Instituicao::create($dados);
+
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function alterar($id, array $dados = [])
+    {
+        try {
+
+            $ponto = Instituicao::where('id', $id)->update($dados);
+
+            return $ponto ;
+
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+    public function remover($id)
+    {
+        try {
+
+            return  Instituicao::findOrFail($id)->delete();
+
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+
+}
