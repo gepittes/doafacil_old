5c7f59929523c6a79e99ac86c63c5e3235f7d526
Jadilson Guedes <jadilson12@gmail.com>
2019-09-18 00:06:39 -0300

    [ADD] Menu configurar minha conta

--- webapp/src/modules/core/AppBar.vue
+++ webapp/src/modules/core/AppBar.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
-        <template  v-if="window.width < 960">
+        <template v-if="window.width < 960">
             <BarraLateral :drawer="drawer"/>
         </template>
 
         <v-app-bar
             :clipped-left="clipped"
+            :height="styleBar.barW"
             app
             dark
-            :height="styleBar.barW"
             class="gradient-doafacil-bg">
 
             <v-container>
-                <v-row align="center" justify="end" v-if="!status.loggedIn">
+                <v-row
+                    v-if="!status.loggedIn"
+                    align="center"
+                    justify="end">
                     <v-col xl="6">
                         <router-link to="/">
                             <v-img
@@ -21,24 +24,33 @@
                         </router-link>
                     </v-col>
 
-                    <v-col lg="7" xl="5" md="8" v-if="window.width > 960">
+                    <v-col
+                        v-if="window.width > 960"
+                        lg="7"
+                        xl="5"
+                        md="8">
                         <v-form
                             ref="form"
                             v-model="valid"
                             @submit.prevent="submit()"
                         >
                             <v-toolbar-items>
-                                <v-row align="center" justify="end">
-                                    <v-col xl="6" lg="6" md="5">
+                                <v-row
+                                    align="center"
+                                    justify="end">
+                                    <v-col
+                                        xl="6"
+                                        lg="6"
+                                        md="5">
                                         <v-text-field
+                                            :rules="emailRules"
+                                            v-model="email"
+                                            :hide-details="true"
                                             color="black"
                                             prepend-icon="fa fa-user"
                                             label="E-mail"
                                             type="email"
                                             name="email"
-                                            :rules="emailRules"
-                                            v-model="email"
-                                            :hide-details="true"
                                             solo
                                             shaped
                                             light
@@ -47,16 +59,19 @@
                                             <span class="caption ml-8 white--text">Cadastrar-se</span>
                                         </router-link>
                                     </v-col>
-                                    <v-col xl="4" lg="4" md="4">
+                                    <v-col
+                                        xl="4"
+                                        lg="4"
+                                        md="4">
                                         <v-text-field
+                                            :rules="passwordRules"
+                                            v-model="password"
+                                            :hide-details="true"
                                             color="black"
                                             prepend-icon="fa fa-lock"
                                             label="Senha"
                                             type="password"
                                             name="password"
-                                            :rules="passwordRules"
-                                            v-model="password"
-                                            :hide-details="true"
                                             solo
                                             shaped
                                             light
@@ -65,7 +80,10 @@
                                             <span class="caption ml-8 white--text">Esqueci a senha</span>
                                         </router-link>
                                     </v-col>
-                                    <v-col xl="2" lg="2" md="2">
+                                    <v-col
+                                        xl="2"
+                                        lg="2"
+                                        md="2">
                                         <v-btn
                                             :disabled="!valid"
                                             class="transparent elevation-0 mb-4"
@@ -78,7 +96,9 @@
                             </v-toolbar-items>
                         </v-form>
                     </v-col>
-                    <v-col align="right" v-else>
+                    <v-col
+                        v-else
+                        align-right>
                         <v-btn
                             class="transparent elevation-0"
                             to="/login">
@@ -88,16 +108,35 @@
                     </v-col>
                 </v-row>
 
-                <v-row align="center" justify="end" v-if="status.loggedIn">
-                    <v-col md="8" lg="6" xl="5">
-                        <v-row align="center" justify="end">
-                            <v-col md="1" lg="2" xl="1" v-if="window.width < 960" >
-                                <v-btn class="transparent elevation-0" @click.stop="drawer = !drawer">
+                <v-row
+                    v-if="status.loggedIn"
+                    align="center"
+                    justify="end">
+                    <v-col
+                        md="8"
+                        lg="6"
+                        xl="5">
+                        <v-row
+                            align="center"
+                            justify="end">
+                            <v-col
+                                v-if="window.width < 960"
+                                md="1"
+                                lg="2"
+                                xl="1" >
+                                <v-btn
+                                    class="transparent elevation-0"
+                                    @click.stop="drawer = !drawer">
                                     <v-icon>fa fa-list</v-icon>
                                 </v-btn>
                             </v-col>
-                            <v-col md="11" lg="10" xl="11">
-                                <router-link to="/" v-if="this.window.width >= 1024">
+                            <v-col
+                                md="11"
+                                lg="10"
+                                xl="11">
+                                <router-link
+                                    v-if="window.width >= 1024"
+                                    to="/">
                                     <v-img
                                         :max-width="styleBar.imgW"
                                         :src="logo"/>
@@ -107,7 +146,11 @@
 
                     </v-col>
 
-                    <v-col md="3" lg="4" xl="4" sm="5">
+                    <v-col
+                        md="3"
+                        lg="4"
+                        xl="4"
+                        sm="5">
                         <v-toolbar-items
                             class="pa-1">
                             <v-menu offset-y>
@@ -123,12 +166,18 @@
                                             <v-list-item-title
                                                 class="user-name"
                                                 text>{{ userName }}
+                                                <v-icon class="white--text">keyboard_arrow_down</v-icon>
                                             </v-list-item-title>
                                         </v-list-item-content>
                                     </v-btn>
 
                                 </template>
                                 <v-list>
+                                    <v-list-item
+                                        to="/configuracao"
+                                    >
+                                        <v-list-item-title>Minha Conta</v-list-item-title>
+                                    </v-list-item>
                                     <v-list-item
                                         to="/main"
                                     >
@@ -150,101 +199,100 @@
 
 </template>
 <script>
-    import {mapGetters, mapActions} from 'vuex';
-    import BarraLateral from "./BarraLateral";
+import { mapGetters, mapActions } from 'vuex';
+// eslint-disable-next-line import/extensions,import/no-unresolved
+import BarraLateral from './BarraLateral';
+// eslint-disable-next-line import/first,import/no-unresolved
+import LogoPng from '@/assets/doafacil/logos/logo-doafacil-bg-white.png';
 
-    export default {
-        name: 'AppBar',
-        components: {BarraLateral},
-        data() {
-            return {
-                window: {
-                    width: 0,
-                    height: 0,
-                },
-                styleBar : {
-                    imgW: 0,
-                    barW: 0
-                },
-                email: '',
-                password: '',
-                clipped: false,
-                valid: true,
-                drawer: false,
-                // eslint-disable-next-line global-require,import/no-unresolved
-                logo: require('@/assets/doafacil/logos/logo-doafacil-bg-white.png'),
-                emailRules: [
-                    v => !!v || 'E-mail obrigatório',
-                    v => /.+@.+/.test(v) || 'E-mail precisa ser válido',
-                ],
-                passwordRules: [v => !!v || 'Senha obrigatória'],
-            };
-        },
-        watch: {
-            'window.width': function (width) {
-                if (width > 960 && this.$route.path === '/login') {
-                    this.$router.push({name: 'home'});
-                }
-                this.window.width = width;
+export default {
+    name: 'AppBar',
+    components: { BarraLateral },
+    data() {
+        return {
+            window: {
+                width: 0,
+                height: 0,
             },
-            'status.loggedIn': function (val) {
-
-                if(this.status.loggedIn) {
-                    this.styleBar.barW = 50;
-                    this.styleBar.imgW = 80;
-                }
-
-                if(this.status.loggedIn !== true){
-                    this.styleBar.barW = 100;
-                    this.styleBar.imgW = 130;
-                }
-
+            styleBar: {
+                imgW: 0,
+                barW: 0,
+            },
+            email: '',
+            password: '',
+            clipped: false,
+            valid: true,
+            drawer: false,
+            logo: LogoPng,
+            emailRules: [
+                v => !!v || 'E-mail obrigatório',
+                v => /.+@.+/.test(v) || 'E-mail precisa ser válido',
+            ],
+            passwordRules: [v => !!v || 'Senha obrigatória'],
+        };
+    },
+    computed: {
+        ...mapGetters({
+            status: 'account/status',
+            user: 'account/user',
+            accountInfo: 'account/accountInfo',
+        }),
+        userName() {
+            return this.accountInfo.name.split(' ')[0];
+        },
+    },
+    watch: {
+        'window.width': function (width) {
+            if (width > 960 && this.$route.path === '/login') {
+                this.$router.push({ name: 'home' });
             }
+            this.window.width = width;
         },
-        methods: {
-            handleResize() {
-                this.window.width = window.innerWidth;
-                this.window.height = window.innerHeight;
-            },
-            submit() {
-                if (this.$refs.form.validate()) {
-                    const {email, password} = this;
-                    if (email && password) {
-                        this.login({email, password});
-                    }
-                }
-            },
-            clear() {
-                this.$refs.form.reset();
-            },
-            ...mapActions('account', ['login', 'logout']),
-        },
-        created() {
-            window.addEventListener('resize', this.handleResize)
-            this.handleResize();
-
-            if(this.status.loggedIn) {
+        'status.loggedIn': function () {
+            if (this.status.loggedIn) {
                 this.styleBar.barW = 50;
                 this.styleBar.imgW = 80;
             }
 
-            if(this.status.loggedIn !== true){
+            if (this.status.loggedIn !== true) {
                 this.styleBar.barW = 100;
                 this.styleBar.imgW = 130;
             }
+        },
+    },
+    created() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
 
+        if (this.status.loggedIn) {
+            this.styleBar.barW = 50;
+            this.styleBar.imgW = 80;
+        }
+
+        if (this.status.loggedIn !== true) {
+            this.styleBar.barW = 100;
+            this.styleBar.imgW = 130;
+        }
+    },
+    methods: {
+        handleResize() {
+            this.window.width = window.innerWidth;
+            this.window.height = window.innerHeight;
         },
-        computed: {
-            ...mapGetters({
-                status: 'account/status',
-                user: 'account/user',
-                accountInfo: 'account/accountInfo',
-            }),
-            userName() {
-                return this.accountInfo.name.split(' ')[0];
-            },
+        submit() {
+            if (this.$refs.form.validate()) {
+                const { email, password } = this;
+                if (email && password) {
+                    this.login({ email, password });
+                }
+            }
         },
-    };
+        clear() {
+            this.$refs.form.reset();
+        },
+        ...mapActions('account', ['login', 'logout']),
+    },
+};
 </script>
 <style>
 
