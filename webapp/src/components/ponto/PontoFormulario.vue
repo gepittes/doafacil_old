6e9806c95aeb797a5079777ca8e0fabfd0d4a088
Jadilson Guedes <jadilson12@gmail.com>
2019-12-12 10:45:50 -0300

    [REMOVE] Removendo  upload de imagem para ponto

--- webapp/src/components/ponto/PontoFormulario.vue
+++ webapp/src/components/ponto/PontoFormulario.vue
@@ -1,231 +1,180 @@
 <template>
-    <v-container>
-        <v-form
-            ref="form"
-            lazy-validation
-            @submit.prevent="salvar()">
-            <v-row justify="center" >
-                <v-col
-                    xl="6"
-                    md="6">
-                    <v-text-field
-                        v-model="ponto.nome"
-                        :rules="[rules.required]"
-                        required
-                        label="Nome do ponto"/>
-                    <v-textarea
-                        v-model="ponto.descricao"
-                        :rules="[rules.required]"
-                        label="Descrição do ponto"
-                    />
-                    <v-row justify="center">
-                        <v-col
-                            cols="12"
-                            xl="6"
-                            md="6">
-                            <v-menu
-                                ref="menuHoraOpen"
-                                v-model="menuHoraOpen"
-                                :close-on-content-click="false"
-                                :nudge-right="40"
-                                :return-value.sync="ponto.hora_open"
-                                transition="scale-transition"
-                                offset-y
-                                full-width
-                                max-width="290px"
-                                min-width="290px"
-                            >
-                                <template v-slot:activator="{ on }">
-                                    <v-text-field
-                                        v-model="ponto.hora_open"
-                                        :rules="[rules.required]"
-                                        label="Horário de Abertura:"
-                                        prepend-icon="access_time"
-                                        readonly
-                                        v-on="on"
-                                    />
-                                </template>
-                                <v-time-picker
-                                    v-if="menuHoraOpen"
-                                    v-model="ponto.hora_open"
-                                    full-width
-                                    format="24hr"
-                                    scrollable
-                                    @click:minute="$refs.menuHoraOpen.save(ponto.hora_open)"
-                                />
-                            </v-menu>
-                        </v-col>
-                        <v-col
-                            md="6"
-                            xl="6" >
-                            <v-menu
-                                ref="menuHoraClose"
-                                v-model="menuHoraClose"
-                                :close-on-content-click="false"
-                                :nudge-right="45"
-                                :return-value.sync="ponto.hora_close"
-                                transition="scale-transition"
-                                offset-y
-                                full-width
-                                max-width="290px"
-                                min-width="290px"
-                            >
-                                <template v-slot:activator="{ on }">
-                                    <v-text-field
-                                        v-model="ponto.hora_close"
-                                        :rules="[rules.required]"
-                                        label="Horário de Fechamento:"
-                                        prepend-icon="access_time"
-                                        readonly
-                                        v-on="on"
-                                    />
-                                </template>
-                                <v-time-picker
-                                    v-if="menuHoraClose"
-                                    v-model="ponto.hora_close"
-                                    full-width
-                                    format="24hr"
-                                    scrollable
-                                    @click:minute="$refs.menuHoraClose.save(ponto.hora_close)"
-                                />
-                            </v-menu>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col>
-                            <strong>Image:</strong>
-                            <input
-                                type="file"
-                                class="form-control"
-                                @change="onImageChange">
-                        </v-col>
-                    </v-row>
-                </v-col>
-                <v-col
-                    xl="6"
-                    md="6">
-                    <v-row justify="center">
-                        <!-- MAP HERE -->
-                        <v-img
-                            src="https://i.imgur.com/O6ZQx8b.png"
-                            width="600"
-                            height="500"/>
-                    </v-row>
-                </v-col>
-            </v-row>
-            <v-row
-                align="center"
-                justify="end">
-                <v-card-actions>
-                    <v-btn
-                        color="secundary"
-                        type="cancel"
-                        class="ma ma-1"
-                        @click="closeDialog()">Fechar
-                    </v-btn>
-                    <v-btn
-                        class="ma ma-1"
-                        color="primary"
-                        type="submit">Salvar
-                    </v-btn>
-                </v-card-actions>
-            </v-row>
-        </v-form>
-    </v-container>
+  <v-container>
+    <v-form ref="form" lazy-validation @submit.prevent="salvar()">
+      <v-row justify="center">
+        <v-col xl="6" md="6">
+          <v-text-field
+            v-model="ponto.nome"
+            :rules="[rules.required]"
+            required
+            label="Nome do ponto"
+          />
+          <v-textarea
+            v-model="ponto.descricao"
+            :rules="[rules.required]"
+            label="Descrição do ponto"
+          />
+          <v-row justify="center">
+            <v-col cols="12" xl="6" md="6">
+              <v-menu
+                ref="menuHoraOpen"
+                v-model="menuHoraOpen"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="ponto.hora_open"
+                transition="scale-transition"
+                offset-y
+                full-width
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="ponto.hora_open"
+                    :rules="[rules.required]"
+                    label="Horário de Abertura:"
+                    prepend-icon="access_time"
+                    readonly
+                    v-on="on"
+                  />
+                </template>
+                <v-time-picker
+                  v-if="menuHoraOpen"
+                  v-model="ponto.hora_open"
+                  full-width
+                  format="24hr"
+                  scrollable
+                  @click:minute="$refs.menuHoraOpen.save(ponto.hora_open)"
+                />
+              </v-menu>
+            </v-col>
+            <v-col md="6" xl="6">
+              <v-menu
+                ref="menuHoraClose"
+                v-model="menuHoraClose"
+                :close-on-content-click="false"
+                :nudge-right="45"
+                :return-value.sync="ponto.hora_close"
+                transition="scale-transition"
+                offset-y
+                full-width
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="ponto.hora_close"
+                    :rules="[rules.required]"
+                    label="Horário de Fechamento:"
+                    prepend-icon="access_time"
+                    readonly
+                    v-on="on"
+                  />
+                </template>
+                <v-time-picker
+                  v-if="menuHoraClose"
+                  v-model="ponto.hora_close"
+                  full-width
+                  format="24hr"
+                  scrollable
+                  @click:minute="$refs.menuHoraClose.save(ponto.hora_close)"
+                />
+              </v-menu>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="end">
+        <v-card-actions>
+          <v-btn
+            color="secundary"
+            type="cancel"
+            class="ma ma-1"
+            @click="closeDialog()"
+            >Fechar
+          </v-btn>
+          <v-btn class="ma ma-1" color="primary" type="submit">Salvar </v-btn>
+        </v-card-actions>
+      </v-row>
+    </v-form>
+  </v-container>
 </template>
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-    name: 'PontoFormulario',
-    // eslint-disable-next-line vue/require-prop-types
-    props: ['instiSelected'],
-    data() {
-        return {
-            menuHoraOpen: false,
-            menuHoraClose: false,
-            submitted: false,
-            ponto: {},
-            rules: { required: value => !!value || 'Campo Obrigatório.' },
+  name: "PontoFormulario",
+  props: ["instiSelected"],
+  data() {
+    return {
+      menuHoraOpen: false,
+      menuHoraClose: false,
+      submitted: false,
+      ponto: {},
+      rules: { required: value => !!value || "Campo Obrigatório." }
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      accountInfo: "account/accountInfo",
+      pontoEditar: "ponto/pontoEditar"
+    })
+  },
+  watch: {
+    pontoEditar: {
+      deep: true,
+      handler(value) {
+        if ("error" in value) {
+          this.ponto = {};
+        } else {
+          this.ponto = { ...value };
+        }
+      }
+    }
+  },
+  created() {
+    this.loadPonto(this.pontoEditar);
+  },
+  methods: {
+    ...mapActions({
+      cadastraPontoDeDoacao: "ponto/cadastraPontoDeDoacao",
+      atualizarPonto: "ponto/atualizarPonto",
+      cleanPontoEditar: "ponto/cleanPontoEditar"
+    }),
+
+    closeDialog() {
+      this.resetValidation();
+      this.reset();
+      this.$emit("closePainel", []);
+    },
+    reset() {
+      this.ponto = {};
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+    salvar() {
+      this.submitted = true;
+      if (this.$refs.form.validate()) {
+        const { ponto } = this;
+        const instituicaoID = {
+          instituicao_id: this.instiSelected
         };
+        const data = Object.assign(ponto, instituicaoID);
+        if (this.pontoEditar.id) {
+          this.atualizarPonto(data);
+          this.cleanPontoEditar({});
+        } else {
+          this.cadastraPontoDeDoacao(data);
+        }
+      }
     },
-
-    computed: {
-        ...mapGetters({
-            accountInfo: 'account/accountInfo',
-            pontoEditar: 'ponto/pontoEditar',
-        }),
-    },
-    watch: {
-        pontoEditar: {
-            deep: true,
-            handler(value) {
-                if ('error' in value) {
-                    this.ponto = {};
-                } else {
-                    this.ponto = { ...value };
-                }
-            },
-        },
-    },
-    created() {
-        this.loadPonto(this.pontoEditar);
-    },
-    methods: {
-        ...mapActions({
-            cadastraPontoDeDoacao: 'ponto/cadastraPontoDeDoacao',
-            atualizarPonto: 'ponto/atualizarPonto',
-            cleanPontoEditar: 'ponto/cleanPontoEditar',
-        }),
-
-        closeDialog() {
-            this.resetValidation();
-            this.reset();
-            this.$emit('closePainel', []);
-        },
-        reset() {
-            this.ponto = {};
-        },
-        resetValidation() {
-            this.$refs.form.resetValidation();
-        },
-        salvar() {
-            this.submitted = true;
-            if (this.$refs.form.validate()) {
-                const { ponto } = this;
-                const instituicaoID = {
-                    instituicao_id: this.instiSelected,
-                };
-                const config = {
-                    headers: { 'content-type': 'multipart/form-data' },
-                }
-                const data = Object.assign(ponto, instituicaoID);
-                if (this.pontoEditar.id) {
-                    this.atualizarPonto(data);
-                    this.cleanPontoEditar({});
-                } else {
-                    this.cadastraPontoDeDoacao(data, config);
-                }
-                // this.closeDialog();
-            }
-        },
-        onImageChange(e) {
-            const files = e.target.files || e.dataTransfer.files;
-            if (!files.length) { return; }
-            this.createImage(files[0]);
-        },
-        createImage(file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                this.ponto.image = e.target.result;
-            };
-            reader.readAsDataURL(file);
-        },
-        loadPonto(value) {
-            if (value) {
-                this.ponto = { ...value };
-            }
-        },
-    },
+    loadPonto(value) {
+      if (value) {
+        this.ponto = { ...value };
+      }
+    }
+  }
 };
 </script>
--- webapp/src/modules/ponto/actions.js
+++ webapp/src/modules/ponto/actions.js
@@ -1,61 +1,86 @@
-import axios from 'axios';
-import * as types from './types';
-import { requisicaoAutorizada } from '../account/_helpers/requisicao-autorizada';
+import axios from "axios";
+import * as types from "./types";
+import { requisicaoAutorizada } from "../account/_helpers/requisicao-autorizada";
 
 export const obterPontoDeDoacoes = ({ dispatch, commit }) => {
-    axios.get('http://localhost/v1/ponto').then((response) => {
-        const { data } = response;
-        commit(types.OBTER_PONTO_DE_DOACOES, data.data);
-    }).catch((error) => {
-        dispatch('alert/error', error.response.data.error, {
-            root: true,
-        });
+  axios
+    .get("http://localhost/v1/ponto")
+    .then(response => {
+      const { data } = response;
+      commit(types.OBTER_PONTO_DE_DOACOES, data.data);
+    })
+    .catch(error => {
+      dispatch("alert/error", error.response.data.error, {
+        root: true
+      });
     });
 };
 
 export const removerPonto = ({ dispatch, commit }, pontoId) => {
-    axios.delete(`http://localhost/v1/ponto/${pontoId}`).then(() => {
-        commit(types.DELETE_PONTO_DE_DOACAO, pontoId);
-        dispatch('alert/success', 'Ponto de Acesso excluido com sucesso!', { root: true });
-    }).catch(() => {
-        dispatch('alert/error', 'Desculpe, Não encontramos este ponto', { root: true });
+  axios
+    .delete(`http://localhost/v1/ponto/${pontoId}`)
+    .then(() => {
+      commit(types.DELETE_PONTO_DE_DOACAO, pontoId);
+      dispatch("alert/success", "Ponto de Acesso excluido com sucesso!", {
+        root: true
+      });
+    })
+    .catch(() => {
+      dispatch("alert/error", "Desculpe, Não encontramos este ponto", {
+        root: true
+      });
     });
 };
 
 export const getPontoByInst = ({ dispatch, commit }, instituicaoId) => {
-    axios.get(`http://localhost/v1/pontoByInst/${instituicaoId}`).then((response) => {
-        const { data } = response;
-        commit(types.GET_PONTO_BY_INSTITUICAO, data.data);
-    }).catch((error) => {
-        dispatch('alert/error', error.response.data.error, {
-            root: true,
-        });
+  axios
+    .get(`http://localhost/v1/pontoByInst/${instituicaoId}`)
+    .then(response => {
+      const { data } = response;
+      commit(types.GET_PONTO_BY_INSTITUICAO, data.data);
+    })
+    .catch(error => {
+      dispatch("alert/error", error.response.data.error, {
+        root: true
+      });
     });
 };
 
-export const cadastraPontoDeDoacao = ({ dispatch, commit }, ponto, config) => axios.post('http://localhost/v1/ponto', ponto, config).then((response) => {
-    const { data } = response;
-    commit(types.ACRESCENTAR_PONTO_DE_DOACAO, data.data);
-    dispatch('alert/success', 'Ponto de acesso criado com sucesso!', { root: true });
-}).catch((error) => {
-    dispatch('alert/error', error.response.data.error, {
-        root: true,
+export const cadastraPontoDeDoacao = ({ dispatch, commit }, ponto) =>
+  axios
+    .post("http://localhost/v1/ponto", ponto, config)
+    .then(response => {
+      const { data } = response;
+      commit(types.ACRESCENTAR_PONTO_DE_DOACAO, data.data);
+      dispatch("alert/success", "Ponto de acesso criado com sucesso!", {
+        root: true
+      });
+    })
+    .catch(error => {
+      dispatch("alert/error", error.response.data.error, {
+        root: true
+      });
     });
-});
 
-export const atualizarPonto = ({ dispatch, commit }, ponto) => requisicaoAutorizada.patch(`http://localhost/v1/ponto/${ponto.id}`, ponto).then(() => {
-    commit(types.ATUALIZAR_PONTO_DE_DOACAO, ponto);
-    dispatch('alert/success', 'Ponto foi atualizado com sucesso!', { root: true });
-}).catch((error) => {
-    dispatch('alert/error', error.response.data.error, {
-        root: true,
+export const atualizarPonto = ({ dispatch, commit }, ponto) =>
+  requisicaoAutorizada
+    .patch(`http://localhost/v1/ponto/${ponto.id}`, ponto)
+    .then(() => {
+      commit(types.ATUALIZAR_PONTO_DE_DOACAO, ponto);
+      dispatch("alert/success", "Ponto foi atualizado com sucesso!", {
+        root: true
+      });
+    })
+    .catch(error => {
+      dispatch("alert/error", error.response.data.error, {
+        root: true
+      });
     });
-});
 
 export const setPontoEditar = ({ commit }, pontoEditar) => {
-    commit(types.SET_PONTO_EDITAR, pontoEditar);
+  commit(types.SET_PONTO_EDITAR, pontoEditar);
 };
 
 export const cleanPontoEditar = ({ commit }, pontoEditar) => {
-    commit(types.CLEAN_PONTO_EDITAR, pontoEditar);
+  commit(types.CLEAN_PONTO_EDITAR, pontoEditar);
 };
